9c96e9f0226daf7fb0168b6e9ebead37b72f18fe
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-12 18:35:36 -0300

    correçao - bug do ingles

--- utills/translator-API.js
+++ utills/translator-API.js
@@ -1,10 +1,11 @@
-const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
-const url = "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/b3c8a014-833b-4505-8ef9-81b155c3dfcb",
-API_KEY = "ufiU3H4-xOBPoAvjWs9Bc1zNsjdyEtz0axB11-M9B-Yu";
+const LanguageTranslatorV3 = require("ibm-watson/language-translator/v3");
+const { IamAuthenticator } = require("ibm-watson/auth");
+const url =
+    "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/b3c8a014-833b-4505-8ef9-81b155c3dfcb",
+  API_KEY = "ufiU3H4-xOBPoAvjWs9Bc1zNsjdyEtz0axB11-M9B-Yu";
 
 const languageTranslator = new LanguageTranslatorV3({
-  version: '2018-05-01',
+  version: "2018-05-01",
   authenticator: new IamAuthenticator({
     apikey: API_KEY,
   }),
@@ -12,52 +13,50 @@ const languageTranslator = new LanguageTranslatorV3({
 });
 
 const params = {
-  text: '',
-  modelId: 'pt-en'
-}
+  text: "",
+  modelId: "pt-en",
+};
 
 const params2 = {
-  text: '',
-  modelId: ''
-}
+  text: "",
+  modelId: "",
+};
 
-
-const translateAPI = async ( comment, source , target) => {
-  let arr =[];
+const translateAPI = async (comment, source, target) => {
+  let arr = [];
 
   const params = {
     text: `${comment}`,
-    modelId: `${source.value}-en`
+    modelId: `${source.value}-en`,
+  };
+
+  await languageTranslator.translate(params).then((response) => {
+    arr.push(response.result.translations[0].translation);
+  });
+  if (target.value === "en") {
+    return arr[0];
+  } else {
+    params2.text = arr[0];
+    params2.modelId = `en-${target.value}`;
+    await languageTranslator.translate(params2).then((response) => {
+      arr.push(response.result.translations[0].translation);
+    });
+    return arr[1];
   }
-  
-  await languageTranslator.translate(params)
-  .then(response => {
-   arr.push(response.result.translations[0].translation);
-  });
+};
 
-  params2.text = arr[0];
-  params2.modelId=`en-${target.value}`
- await languageTranslator.translate(params2)
-  .then(response => {
-   arr.push(response.result.translations[0].translation);
-  });
-  return arr[1];
-}
+// languageTranslator.listLanguages()
+// .then(languages => {
+//   console.log(JSON.stringify(languages, null, 2))
+//   const arrOfLanguages = languages.result.languages;
+//   console.log(JSON.stringify(arrOfLanguages, null, 2));
+//   // console.log(arrOfLanguages.map((item) => item.language +" " + item.language_name))
+// })
+// .catch(err => {
+//   console.log('error:', err);
+// });
 
-  
-  // languageTranslator.listLanguages()
-  // .then(languages => {
-  //   console.log(JSON.stringify(languages, null, 2))
-  //   const arrOfLanguages = languages.result.languages;
-  //   console.log(JSON.stringify(arrOfLanguages, null, 2));
-  //   // console.log(arrOfLanguages.map((item) => item.language +" " + item.language_name))
-  // })
-  // .catch(err => {
-  //   console.log('error:', err);
-  // });
-
-  // linguas suportadas pelo text-to-speech
-  // english, french, german, italian, japanese, korean, portuguese, spanish
-
+// linguas suportadas pelo text-to-speech
+// english, french, german, italian, japanese, korean, portuguese, spanish
 
 module.exports = translateAPI;
