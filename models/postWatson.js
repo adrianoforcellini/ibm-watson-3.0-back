8dd8e00a69230f0c8d865a28ac699597381b9e9e
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-12 15:11:11 -0300

    criaçao de audios , rota unica a partir do translate

--- models/postWatson.js
+++ models/postWatson.js
@@ -1,26 +1,25 @@
-const synthesizeAudio = require('../utills/text-to-speech-API');
+const synthesizeAudio = require("../utills/text-to-speech-API");
 
-const postWatson = async (req, res) => {
-  const {id, comment} =  req.body;
-  const voices = {
-   de : 'de-DE_DieterV3Voice',
-   en : 'en-GB_JamesV3Voice',
-   it : 'it-IT_FrancescaV3Voice',
-   fr : 'fr-FR_ReneeV3Voice',
-   ja : 'ja-JP_EmiV3Voice',
-   ko : 'ko-KR_HyunjunVoice',
-   es : 'es-ES_LauraV3Voice',
-   pt : 'pt-BR_IsabelaV3Voice',
-  }
-  const synthesizeParams =
-  {
-    text: `${comment}`,
-    accept: 'audio/mp3',
-    voice: 'pt-BR_IsabelaV3Voice',
+const postWatson = async (translate, value, commentId) => {
+  const voices = [
+    { id: "de", name: "de-DE_DieterV3Voice" },
+    { id: "en", name: "en-GB_JamesV3Voice" },
+    { id: "it", name: "it-IT_FrancescaV3Voice" },
+    { id: "fr", name: "fr-FR_ReneeV3Voice" },
+    { id: "ja", name: "ja-JP_EmiV3Voice" },
+    { id: "ko", name: "ko-KR_HyunjunVoice" },
+    { id: "es", name: "es-ES_LauraV3Voice" },
+    { id: "pt", name: "pt-BR_IsabelaV3Voice" },
+  ];
+
+  const voice = voices.filter((item) => item.id === value)[0].name;
+
+  const synthesizeParams = {
+    text: `${translate}`,
+    accept: "audio/mp3",
+    voice: voice,
   };
-  
-  synthesizeAudio(synthesizeParams, id);
-  res.send(comment);
+  synthesizeAudio(synthesizeParams, commentId);
 };
 
 module.exports = postWatson;
--- models/translator.js
+++ models/translator.js
@@ -1,33 +1,14 @@
 const translatorAPI = require("../utills/translator-API");
-const synthesizeAudio = require("../utills/text-to-speech-API");
+const postWatson = require("./postWatson");
+const insert = require("./insert");
 
 const translator = async (req, _res) => {
-  const { comment, source, target } = req.body;
+  const { comment, source, target, commentId } = req.body;
   const { value } = target;
-  console.log(value);
 
-  const translate = await translatorAPI(comment, source, target);
-  const voices = [
-    { id: "de", name: "de-DE_DieterV3Voice" },
-    { id: "en", name: "en-GB_JamesV3Voice" },
-    { id: "it", name: "it-IT_FrancescaV3Voice" },
-    { id: "fr", name: "fr-FR_ReneeV3Voice" },
-    { id: "ja", name: "ja-JP_EmiV3Voice" },
-    { id: "ko", name: "ko-KR_HyunjunVoice" },
-    { id: "es", name: "es-ES_LauraV3Voice" },
-    { id: "pt", name: "pt-BR_IsabelaV3Voice" },
-  ];
-
-  const voice = voices.filter(item => item.id === value)[0].name
-
-   const synthesizeParams =
-   {
-     text: `${translate}`,
-     accept: 'audio/mp3',
-     voice: voice,
-   };
-   console.log(translate)
-   synthesizeAudio(synthesizeParams, 2);
+  const translate = await translatorAPI(comment, source, target, commentId);
+   insert(commentId, translate);
+   postWatson(comment,value, commentId);
 };
 
 module.exports = translator;
