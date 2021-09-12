b6921b0104809d9f94e0368c9f26aa6a809acc56
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-12 15:28:54 -0300

    .

--- models/translator.js
+++ models/translator.js
@@ -2,13 +2,14 @@ const translatorAPI = require("../utills/translator-API");
 const postWatson = require("./postWatson");
 const insert = require("./insert");
 
-const translator = async (req, _res) => {
+const translator = async (req, res) => {
   const { comment, source, target, commentId } = req.body;
   const { value } = target;
 
   const translate = await translatorAPI(comment, source, target, commentId);
    insert(commentId, translate);
    postWatson(comment,value, commentId);
+   res.status(200).send(translate);
 };
 
 module.exports = translator;
