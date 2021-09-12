0d16ff3e22d9c66fc25291445f079c507ef49ee1
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-12 12:16:05 -0300

    vozes em text_to_speech

--- models/postWatson.js
+++ models/postWatson.js
@@ -2,6 +2,16 @@ const synthesizeAudio = require('../utills/text-to-speech-API');
 
 const postWatson = async (req, res) => {
   const {id, comment} =  req.body;
+  const voices = {
+   de : 'de-DE_DieterV3Voice',
+   en : 'en-GB_JamesV3Voice',
+   it : 'it-IT_FrancescaV3Voice',
+   fr : 'fr-FR_ReneeV3Voice',
+   ja : 'ja-JP_EmiV3Voice',
+   ko : 'ko-KR_HyunjunVoice',
+   es : 'es-ES_LauraV3Voice',
+   pt : 'pt-BR_IsabelaV3Voice',
+  }
   const synthesizeParams =
   {
     text: `${comment}`,
